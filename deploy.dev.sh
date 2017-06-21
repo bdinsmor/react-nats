b1b5888460db9214433e478006dfc0310112efd3
David Culverhouse <david.culverhouse@gmail.com>
2017-06-21 07:24:16 -0400

    devops

---
+++ deploy.dev.sh
@@ -0,0 +1,4 @@
+eslint --ignore-pattern '/public/lib/' public/*.js
+
+aws s3 sync ./public/ s3://analyst-dev.pricedigestsapi.com  --profile pricedigests-deploy
+aws s3 cp ./env.dev.js s3://analyst-dev.pricedigestsapi.com/env.js  --profile pricedigests-deploy
