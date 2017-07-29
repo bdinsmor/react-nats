2a68b8f839c84942b0f83559660f503969293004
David Culverhouse <david.culverhouse@gmail.com>
2017-07-28 22:20:16 -0400

    updating local env

--- env.local.js
+++ env.local.js
@@ -1,5 +1,6 @@
 angular.module('PriceDigests').constant("ENV", {
-    "API_URL": "https://internal-dev.pricedigestsapi.com",
-    "TEST_URL": "https://dev.pricedigestsapi.com/test"
-        // "API_URL": "http://localhost:8082"
+    // "API_URL": "https://internal-dev.pricedigestsapi.com",
+    // "TEST_URL": "https://dev.pricedigestsapi.com/test",
+    "TEST_URL": "https://localhost:8082/test",
+    "API_URL": "http://localhost:8082"
 });
